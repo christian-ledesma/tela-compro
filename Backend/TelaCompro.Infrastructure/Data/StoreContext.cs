3dde3743eed5ff2baeaeb541e0c2954cf8a897f9
Christian <christian_ledesma_a@hotmail.com>
2024-02-04 01:03:01 +0100

    Add entity framework and context

---
+++ Backend/TelaCompro.Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TelaCompro.Domain.Entities;
+
+namespace TelaCompro.Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ Backend/TelaCompro.Infrastructure/Extensions/InfrastructureServiceCollection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TelaCompro.Infrastructure.Data;
+
+namespace TelaCompro.Infrastructure.Extensions
+{
+    public static class InfrastructureServiceCollection
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("StoreServer"));
+            });
+            return services;
+        }
+    }
+}
