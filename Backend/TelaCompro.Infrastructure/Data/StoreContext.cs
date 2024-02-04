1c11cb26ff4bd146c98f8fde58149c708ea3f624
Christian <christian_ledesma_a@hotmail.com>
2024-02-04 01:48:36 +0100

    Add entities relation configuration

--- Backend/TelaCompro.Infrastructure/Data/StoreContext.cs
+++ Backend/TelaCompro.Infrastructure/Data/StoreContext.cs
@@ -15,5 +15,20 @@ namespace TelaCompro.Infrastructure.Data
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Product>()
+                .HasMany(x => x.Tags)
+                .WithMany(x => x.Products);
+
+            modelBuilder
+                .Entity<User>()
+                .HasMany(x => x.Products)
+                .WithOne(x => x.Owner);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
