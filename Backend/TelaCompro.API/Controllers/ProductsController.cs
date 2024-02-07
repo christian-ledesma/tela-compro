dc994d20181fd164f73d67c50eab17aabb5a4673
Christian <christian_ledesma_a@hotmail.com>
2024-02-07 23:42:10 +0100

    Add users and products controllers

---
+++ Backend/TelaCompro.API/Controllers/ProductsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TelaCompro.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
