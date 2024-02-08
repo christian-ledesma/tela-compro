a03ca689a02a0d3bd6b0e5d5256e32f24c8b5503
Christian <christian_ledesma_a@hotmail.com>
2024-02-08 01:42:20 +0100

    Fix data seeding

--- Backend/TelaCompro.Infrastructure/Extensions/InfrastructureServiceCollection.cs
+++ Backend/TelaCompro.Infrastructure/Extensions/InfrastructureServiceCollection.cs
@@ -63,28 +63,6 @@ namespace TelaCompro.Infrastructure.Extensions
 
             context.Categories.AddRange(categories);
 
-            var users = new List<User>
-            {
-                new()
-                {
-                    Name = "Christian",
-                    FirstLastName = "Ledesma",
-                    SecondLastName = "Aguilar",
-                    Email = "christian.ledesma@telacompro.com",
-                    PhoneNumber = "+34 675517349"
-                },
-                new()
-                {
-                    Name = "Omar",
-                    FirstLastName = "Ledesma",
-                    SecondLastName = "Aguilar",
-                    Email = "omar.ledesma@telacompro.com",
-                    PhoneNumber = "+34 675117609"
-                }
-            };
-
-            context.Users.AddRange(users);
-
             var tags = new List<Tag>
             {
                 new()
@@ -98,23 +76,6 @@ namespace TelaCompro.Infrastructure.Extensions
             };
 
             context.Tags.AddRange(tags);
-
-            var products = new List<Product>
-            {
-                new()
-                {
-                    Name = "Camiseta Hollister",
-                    Description = "Se vende camiseta original",
-                    Size = "M",
-                    Price = 15.99M,
-                    Owner = users.First(x => x.Name == "Christian"),
-                    Category = categories.First(x => x.Name == "Shirts"),
-                    Brand = brands.First(x => x.Name == "Hollister"),
-                    Tags = tags
-                }
-            };
-
-            context.Products.AddRange(products);
 
             context.SaveChanges();
         }
